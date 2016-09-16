8ca87552742c4d52b5f8e79199e9bf5501c0fb05
Cory Collier <cory.collier@ahss.org>
2016-09-16 09:40:16 -0400

    Adding code climate test coverage support, and adding a build-status image to the README file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # corycollier.com
+
+[![Build Status](https://travis-ci.org/corycollier/corycollier.com.svg?branch=develop)](https://travis-ci.org/corycollier/corycollier.com)
+
 My public website.
--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -13,12 +13,12 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class ScriptHandler {
 
-  protected static function getDrupalRoot($project_root) {
-    return $project_root . '/web';
+  protected static function getDrupalRoot($projectRoot) {
+    return $projectRoot . '/web';
   }
 
   public static function createRequiredFiles(Event $event) {
-    $fs = new Filesystem();
+    $filesystem = new Filesystem();
     $root = static::getDrupalRoot(getcwd());
 
     $dirs = [
@@ -29,30 +29,30 @@ class ScriptHandler {
 
     // Required for unit testing
     foreach ($dirs as $dir) {
-      if (!$fs->exists($root . '/'. $dir)) {
-        $fs->mkdir($root . '/'. $dir);
-        $fs->touch($root . '/'. $dir . '/.gitkeep');
+      if (!$filesystem->exists($root . '/'. $dir)) {
+        $filesystem->mkdir($root . '/'. $dir);
+        $filesystem->touch($root . '/'. $dir . '/.gitkeep');
       }
     }
 
     // Prepare the settings file for installation
-    if (!$fs->exists($root . '/sites/default/settings.php') and $fs->exists($root . '/sites/default/default.settings.php')) {
-      $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
-      $fs->chmod($root . '/sites/default/settings.php', 0666);
+    if (!$filesystem->exists($root . '/sites/default/settings.php') and $filesystem->exists($root . '/sites/default/default.settings.php')) {
+      $filesystem->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
+      $filesystem->chmod($root . '/sites/default/settings.php', 0666);
       $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
     }
 
     // Prepare the services file for installation
-    if (!$fs->exists($root . '/sites/default/services.yml') and $fs->exists($root . '/sites/default/default.services.yml')) {
-      $fs->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
-      $fs->chmod($root . '/sites/default/services.yml', 0666);
+    if (!$filesystem->exists($root . '/sites/default/services.yml') and $filesystem->exists($root . '/sites/default/default.services.yml')) {
+      $filesystem->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
+      $filesystem->chmod($root . '/sites/default/services.yml', 0666);
       $event->getIO()->write("Create a sites/default/services.yml file with chmod 0666");
     }
 
     // Create the files directory with chmod 0777
-    if (!$fs->exists($root . '/sites/default/files')) {
+    if (!$filesystem->exists($root . '/sites/default/files')) {
       $oldmask = umask(0);
-      $fs->mkdir($root . '/sites/default/files', 0777);
+      $filesystem->mkdir($root . '/sites/default/files', 0777);
       umask($oldmask);
       $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
     }
